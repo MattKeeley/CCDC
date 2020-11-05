5340ab0c93b02c44ffaa82d918ddc234ca3a7bf4
Matt Keeley <mrkeeley@asu.edu>
2020-11-05 09:30:45 -0700

    Update mjolnir.ps1

--- mjolnir.ps1
+++ mjolnir.ps1
@@ -2,3 +2,4 @@ $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("https://raw.githubusercontent.com/MattKeeley/CCDC/master/Hammer.cmd","C:\hammer.cmd")
 $WebClient.DownloadFile("https://github.com/MattKeeley/CCDC/raw/master/PatchMyPC.exe","C:\patchmypc.exe")
 $WebClient.DownloadFile("https://github.com/MattKeeley/CCDC/raw/master/MBSetup.exe","C:\malwarebytes.exe")
+$WebClient.DownloadFile("https://nmap.org/dist/nmap-7.91-setup.exe","C:\nmap.exe")
